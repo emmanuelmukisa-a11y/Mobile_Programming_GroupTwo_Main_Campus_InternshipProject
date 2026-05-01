198dd0afd653eb67cf582decc236683888fbb9b3
Apako Treasure <apakotreasure@gmail.com>
2026-05-01 11:07:46 +0300

    login screen and component package complete

--- app/src/main/java/com/ndejje/momo/ourinternship/ui/components/CommonUI.kt
+++ app/src/main/java/com/ndejje/momo/ourinternship/ui/components/CommonUI.kt
@@ -7,7 +7,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.PasswordVisualTransformation
-import com.ndejje.internee.R
+import com.ndejje.momo.ourinternship.R
 
 @Composable
 fun AppTextField(
